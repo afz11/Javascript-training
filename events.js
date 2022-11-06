4beb6916f056abe57131b4e41c3c2276f914724c
Abo <abozarafzali11@gmail.com>
2022-11-06 14:26:14 +0100

    Event Bubbling

--- events.js
+++ events.js
@@ -4,17 +4,40 @@ const submit = document.querySelector('#submit');
 const itemCounter = document.querySelector('h2 b'); 
 const title = document.querySelector('.main-title');
 const titleChangerDiv = document.querySelector('.title-changer-div');
+const nameInput = document.querySelector('.name-input');
 
 
 
 
-submit.addEventListener('click', () => {
+submit.addEventListener('click', event => {
+    event.preventDefault();
+    //Create element
     const newItem = document.createElement("li");
+    //Adding Class
     newItem.classList.add("item");
-    newItem.innerText = `Item ${items.length + 1}`;
+    // Adding text
+    newItem.innerText = nameInput.value;
+    
     todoList.appendChild(newItem);
-    itemCounter.innerText = `${items.length}`
+    itemCounter.innerText = `${items.length}`;
+    //Delete value from the input
+    nameInput.value = '';
+    //Create the element and attaching the listener
+    newItem.addEventListener('click', deleteItem);
+
+    console.log(nameInput)
 });
+
+
+
+function deleteItem(event) {
+    event.stopPropagation(); // Stop Event Bubbling!!
+    event.target.remove();
+}
+
+todoList.addEventListener('click', ()=> {
+    todoList.classList.toggle('fade');
+})
 
 //Function declaration 
 
@@ -27,23 +50,19 @@ submit.addEventListener('click', () => {
 // }
 
 
-function addButton(className, name, parent) {
-    const newButton = document.createElement('button');
-    newButton.classList.add(className);
-    newButton.innerText = name;
-    titleChangerDiv.appendChild(newButton);
-}
+// function addButton(className, name, parent) {
+//     const newButton = document.createElement('button');
+//     newButton.classList.add(className);
+//     newButton.innerText = name;
+//     titleChangerDiv.appendChild(newButton);
+// }
 
-// Create new Button 
-addButton("title-changer", "Click me!", titleChangerDiv);
-const changeButton = document.querySelector('.title-changer');
+// // Create new Button 
+// addButton("title-changer", "Click me!", titleChangerDiv);
+// const changeButton = document.querySelector('.title-changer');
 
-changeButton.addEventListener("keydown", function(e) {
-    console.log(e.keyCode);
-    if (e.keyCode === 81) {
-        title.classList.toggle('big-and-red');
-    }
-
-});
+// changeButton.addEventListener("click", function() {
+//     title.classList.toggle('big-and-red');
+// });
 
 

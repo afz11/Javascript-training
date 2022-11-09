0b9c3d138e4a72b69980aee700c3b7ae2f94c39a
Abo <abozarafzali11@gmail.com>
2022-11-09 10:26:29 +0100

    BeatMaker Added css

---
+++ beatMakerPorject/app.js
@@ -0,0 +1,11 @@
+class DrumKit{
+    constructor(){
+        this.pads = document.querySelectorAll('.pad');
+        this.kickAudio = document.querySelector('.kick-sound');
+        this.hihatAudio = document.querySelector('.hihat-sound');
+        this.snareAudio = document.querySelector('.snare-sound');
+        this.index = 0;
+    }
+
+    
+}

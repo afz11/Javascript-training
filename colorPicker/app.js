bfec103914dca29256588d1a250c3bc18aa53e3b
Abo <abozarafzali11@gmail.com>
2022-11-11 19:55:35 +0100

    colorPicker project html and css added

---
+++ colorPicker/app.js
@@ -0,0 +1,4 @@
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHaxes = document.querySelectorAll('.color h2');
